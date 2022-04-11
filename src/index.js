93c0058e16a56228c5bd37cd30ddd48c2c47713b
Are Manish Reddy <54163952+manishreddy255@users.noreply.github.com>
2022-04-11 16:11:32 +0530

    added webpack config and bable

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ const elevenShieldRecipe = {
     ironIngot: 1
 }
 
+const anotherRecipie = {
+    ...elevenShieldRecipe,
+    wool: 8989,
+    colorfool: 9898,
+    refinedMoonStone: 4
+}
+
 console.log(elevenShieldRecipe)
+
+console.log(anotherRecipie)
